45bf376c8cb5182970bcf81bdf259a55255c7fce
Jacek Wielemborek <d33tah@gmail.com>
2021-02-27 21:05:44 +0100

    Refactor, fix bugs related to gap election and handling of gap input.

--- templates/index.js
+++ templates/index.js
@@ -1,112 +1,106 @@
 function main() {
-	/* Loads all the js libraries and project modules, then calls onReady. */
-	// var pathRoot = './lib/';
-	console.log('main()');
-	head.js();
-	head.ready(onReady);
+    /* Loads all the js libraries and project modules, then calls onReady. */
+    console.log('main()');
+    head.js();
+    head.ready(onReady);
 }
 
-
-
-
 function getRandomInt(min, max) {
-	return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function electGap(current_text) {
+    do {
+        gapIndex = getRandomInt(0, current_text.length - 1);
+    } while (current_text[gapIndex] == ".");
+    return gapIndex;
+}
+
+function buildTbox(current_text) {
+    spans = '';
+    gap = electGap(current_text);
+    for (var i = 0; i < current_text.length; i++) {
+        if (i == gap) {
+
+            spans += ' <span onBlur="checkInput(\'t' + i + '\');" id="t';
+            spans += i + '" style="border-bottom: thin dotted #000000;" data-value="';
+            spans += current_text[i] + '" contenteditable>' + '&nbsp;&nbsp;&nbsp;' + '</span> ';
+        } else {
+            spans += '<span>' + current_text[i] + '</span> '
+        }
+    }
+    return spans;
 }
 
 function onReady() {
-	console.log('onReady()');
-	var questions = {};
-	var xhr = new XMLHttpRequest();
-	xhr.onreadystatechange = function() {
-		if (xhr.readyState === 4){
-			res = JSON.parse(xhr.responseText);
-			console.log(res);
-			console.log(res["questions"]);
-			console.log(res["questions"][0]);
-			current_question = res["questions"][0];
-			current_audio = current_question["path"];
-			current_text = current_question["tokenized"];
-			//   res = httpGet('/static/cv-corpus-6.1-2020-12-11/fi/clips/common_voice_fi_23997016.mp3');
-			var player = document.getElementById('player');
-			var source = document.getElementById('audioSource');
+    var questions = {};
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState !== 4) {
+            return;
+        }
+        res = JSON.parse(xhr.responseText);
+        current_question = res["questions"][0];
+        current_audio = current_question["path"];
+        current_text = current_question["tokenized"];
+        var player = document.getElementById('player');
+        var source = document.getElementById('audioSource');
 
-			source.src = '/static/cv-corpus-6.1-2020-12-11/' + current_question['locale'] + '/clips/' + current_audio;
-			source.type = 'audio/mp3';
-			player.load();
-			var tbox = document.getElementById('textbox');
-			spans = ''
-				gap = getRandomInt(0, current_text.length - 1);
-			for(var i = 0; i < current_text.length; i++) {
-				if(i == gap)  {
+        source.src = '/static/cv-corpus-6.1-2020-12-11/' + current_question['locale'] + '/clips/' + current_audio;
+        source.type = 'audio/mp3';
+        player.load();
+        var tbox = document.getElementById('textbox');
+        tbox.innerHTML = buildTbox(current_text);
 
-					//			$("#sent-" + row[0]).append('<span onBlur="checkInput(\''+tokenId+'\');" onKeyUp="userInput(\''+tokenId+'\');" id="token-' + tokenId + '" data-value="' + t + '" alt="' + t + '" contenteditable>' + t + '</span> ');
-
-					spans += ' <span onBlur="checkInput(\'t'+i+'\');" id="t' + i + '" style="border-bottom: thin dotted #000000;" data-value="' + current_text[i] + '" contenteditable>' + '&nbsp;&nbsp;&nbsp;'  + '</span> '  
-				} else {
-					spans += ' <span>' + current_text[i] + '</span> '  
-				}
-			}
-			tbox.innerHTML = spans;
-
-		}
-	};
-	xhr.open('GET', 'http://localhost:5001/get_clips');
-	xhr.send();
+    };
+    xhr.open('GET', 'http://localhost:5001/get_clips');
+    xhr.send();
 
 }
 
 function checkInput(tid) {
-	console.log('checkInput() ' + tid);
-	span = document.getElementById(tid);
+    console.log('checkInput() ' + tid);
+    span = document.getElementById(tid);
 
-	correct = span.getAttribute("data-value");
-	guess = span.textContent;
-	guess = guess.trim();
+    correct = span.getAttribute("data-value");
+    guess = span.childNodes[0].textContent.replaceAll(/\s/g,'');
+    guess = guess.trim();
 
-	console.log('correct: ' + correct);
-	console.log('guess: ' + guess);
+    console.log('correct: ' + correct);
+    console.log('guess: ' + guess);
 
-	if(guess == correct) {
-             console.log('CORRECT!');
-             span.setAttribute("style", "color: green");
-	} else {
-             console.log('INCORRECT!');
-             span.setAttribute("style", "color: red");
-             var shouldBe = document.createElement("span");
-	     shouldBe.setAttribute("style", "color:green");
-             var t = document.createTextNode(" [" + correct + "]");
-             shouldBe.appendChild(t);
-             span.appendChild(shouldBe);
-	}
-}
-
-function parseText(text) {
-	console.log(text);
+    if (guess == correct) {
+        console.log('CORRECT!');
+        span.setAttribute("style", "color: green");
+        if (span.childNodes.length != 1) {
+            span.childNodes[1].remove();
+        }
+    } else {
+        console.log('INCORRECT!');
+        span.setAttribute("style", "color: red");
+        var shouldBe = document.createElement("span");
+        shouldBe.setAttribute("style", "color:green");
+        var t = document.createTextNode(" [" + correct + "]");
+        shouldBe.appendChild(t);
+        span.appendChild(shouldBe);
+    }
 }
 
 function httpGet(theUrl) {
-	console.log('httpGet()' + theUrl);
-	var xmlhttp;
-	if (window.XMLHttpRequest)
-	{// code for IE7+, Firefox, Chrome, Opera, Safari
-		xmlhttp=new XMLHttpRequest();
-	}
-	else
-	{// code for IE6, IE5
-		console.log('shit outta luck');
-	}
-	xmlhttp.onreadystatechange=function()
-	{
-		if (xmlhttp.readyState==4 && xmlhttp.status==200)
-		{
-			return xmlhttp.responseText;
-			//parseText(xmlhttp.responseText);
-		}
-	}
-	xmlhttp.open("GET", theUrl, false);
-	xmlhttp.send();	
+    console.log('httpGet()' + theUrl);
+    var xmlhttp;
+    if (window.XMLHttpRequest) { // code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp = new XMLHttpRequest();
+    } else { // code for IE6, IE5
+        alert('This browser is not supported or has insufficient permissions.');
+    }
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            return xmlhttp.responseText;
+        }
+    }
+    xmlhttp.open("GET", theUrl, false);
+    xmlhttp.send();
 }
 
-
 window.onload = main;
-
