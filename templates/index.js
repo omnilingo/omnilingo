ddd1f5547aad25f42930a1ee7fdaaa94fb51a7da
Francis Tyers <ftyers@prompsit.com>
2021-02-27 17:58:15 +0000

    audio loading now work

--- templates/index.js
+++ templates/index.js
@@ -10,9 +10,20 @@ head.ready(onReady);
 
 function onReady() {
    console.log('onReady()');
-   res = httpGet('/static/cv-corpus-6.1-2020-12-11/fi/clips/common_voice_fi_23997016.mp3');
+
+//   res = httpGet('/static/cv-corpus-6.1-2020-12-11/fi/clips/common_voice_fi_23997016.mp3');
+   var player = document.getElementById('player');
+   var source = document.getElementById('audioSource');
+
+   source.src = 'http://localhost:5001/static/cv-corpus-6.1-2020-12-11/fi/clips/common_voice_fi_23997016.mp3';
+   source.type = 'audio/mp3';
+   player.load();
+
 }
 
+function parseText(text) {
+ console.log(text);
+}
 
 function httpGet(theUrl) {
 	console.log('httpGet()' + theUrl);
@@ -29,7 +40,8 @@ xmlhttp.onreadystatechange=function()
 {
 if (xmlhttp.readyState==4 && xmlhttp.status==200)
 {
-parseText(xmlhttp.responseText);
+return xmlhttp.responseText;
+//parseText(xmlhttp.responseText);
 }
 }
 xmlhttp.open("GET", theUrl, false);
