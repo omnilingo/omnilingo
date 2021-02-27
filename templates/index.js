6ddecb2ade09728be2fc899c3f8f93dfaab579bf
Francis Tyers <ftyers@prompsit.com>
2021-02-27 20:26:01 +0000

    remove double onready

--- templates/index.js
+++ templates/index.js
@@ -9,46 +9,6 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function onReady() {
-	console.log('onReady()');
-	var questions = {};
-	var xhr = new XMLHttpRequest();
-	xhr.onreadystatechange = function() {
-		if (xhr.readyState === 4){
-			res = JSON.parse(xhr.responseText);
-			console.log(res);
-			console.log(res["questions"]);
-			console.log(res["questions"][0]);
-			current_question = res["questions"][0];
-			current_audio = current_question["path"];
-			current_text = current_question["tokenized"];
-			//   res = httpGet('/static/cv-corpus-6.1-2020-12-11/fi/clips/common_voice_fi_23997016.mp3');
-			var player = document.getElementById('player');
-			var source = document.getElementById('audioSource');
-
-			source.src = '/static/cv-corpus-6.1-2020-12-11/' + current_question['locale'] + '/clips/' + current_audio;
-			source.type = 'audio/mp3';
-			player.load();
-			var tbox = document.getElementById('textbox');
-			spans = ''
-				gap = getRandomInt(0, current_text.length - 1);
-			for(var i = 0; i < current_text.length; i++) {
-				if(i == gap)  {
-
-					//			$("#sent-" + row[0]).append('<span onBlur="checkInput(\''+tokenId+'\');" onKeyUp="userInput(\''+tokenId+'\');" id="token-' + tokenId + '" data-value="' + t + '" alt="' + t + '" contenteditable>' + t + '</span> ');
-
-					spans += ' <span onBlur="checkInput(\'t'+i+'\');" id="t' + i + '" style="border-bottom: thin dotted #000000;" data-value="' + current_text[i] + '" contenteditable>' + '&nbsp;&nbsp;&nbsp;'  + '</span> '  
-				} else {
-					spans += ' <span>' + current_text[i] + '</span> '  
-				}
-			}
-			tbox.innerHTML = spans;
-
-		}
-	};
-	xhr.open('GET', 'http://localhost:5001/get_clips?nlevels=10&level=0');
-	xhr.send();
-}
 function electGap(current_text) {
     do {
         gapIndex = getRandomInt(0, current_text.length - 1);
@@ -93,7 +53,8 @@ function onReady() {
         tbox.innerHTML = buildTbox(current_text);
 
     };
-    xhr.open('GET', 'http://localhost:5001/get_clips');
+    
+    xhr.open('GET', 'http://localhost:5001/get_clips?nlevels=10&level=0');
     xhr.send();
 
 }
