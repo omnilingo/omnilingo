d5e1d61a6bd57616a195d29ae6230eb2ed99d56f
Jacek Wielemborek <d33tah@gmail.com>
2021-03-07 20:46:54 +0100

    Reformat, add feedback functions.

--- templates/index.py
+++ templates/index.py
@@ -2,6 +2,7 @@ import json
 
 import browser
 import browser.ajax
+import browser.local_storage
 
 
 def on_audio_ended(_ev):
@@ -12,37 +13,72 @@ def on_audio_ended(_ev):
 
 
 def on_correct_answer():
-    pass  # TODO
+    responses = browser.local_storage.storage.get("responses", "")
+    responses += "-"
+    browser.local_storage.storage["responses"] = responses
+
 
 def on_incorrect_answer():
-    pass  # TODO
+    responses = browser.local_storage.storage.get("responses", "")
+    responses += "-"
+    browser.local_storage.storage["responses"] = responses
+
 
 def clear_feedback():
-    pass  # TODO
+    my_node = browser.document.getElementById("feedback")
+    while my_node.firstChild:
+        my_node.removeChild(my_node.firstChild)
+
 
 def draw_feedback():
-    pass  # TODO
+    responses = browser.local_storage.storage.get("responses", "?" * 10)
+    feedback = browser.document.getElementById("feedback")
+    style = "padding:2px;align:center;border: 1px solid black"
+    for i in range(10):
+        span = browser.document.createElement("span")
+        if i > len(responses) - 1:
+            t = browser.document.createTextNode(" ? ")
+            span["style"] = style + ";color: white;"
+            span.appendChild(t)
+        elif responses[i] == "-":
+            t = browser.document.createTextNode(" ✘ ")
+            span["style"] = style + ";color: red;"
+            span.appendChild(t)
+        elif responses[i] == "+":
+            t = browser.document.createTextNode(" ✔ ")
+            span["style"] = style + ";color: green;"
+            span.appendChild(t)
+        feedback.attach(span)
+        padding = browser.document.createElement("span")
+        padding["style"] = "width: 20px"
+        just_space = browser.document.createTextNode(" ")
+        padding.attach(just_space)
+        feedback.attach(padding)
+
 
 def draw_languages():
     pass  # TODO
+
 
 def get_clip_and_then(fn):
     def process_and_call(ajax_obj):
         j = json.loads(ajax_obj.read())
         fn(j)
-    url = '/get_clips?nlevels=10&enabled=blanks&level=1&language=pl'
+
+    url = "/get_clips?nlevels=10&enabled=blanks&level=1&language=pl"
     return browser.ajax.get(url, mode="text", oncomplete=process_and_call)
+
 
 def draw_answer(active_input):
     guess = active_input.value
-    correct = active_input['data-value']
+    correct = active_input["data-value"]
     # TODO: improve styling, e.g. variable name should be like_this
     # TODO: also, maybe split it into two functions? also, can't we just
     # TODO: access the parent node by ID?
     if guess.lower() == correct.lower():
         on_correct_answer()
-        answer = browser.document.createElement('span')
-        answer['class'] = 'correct'
+        answer = browser.document.createElement("span")
+        answer["class"] = "correct"
         answer_text_node = browser.document.createTextNode(correct + " ")
         answer.attach(answer_text_node)
         active_input.parentNode.insertBefore(answer, active_input.nextSibling)
@@ -71,50 +107,54 @@ def draw_answer(active_input):
 
 
 def user_input(ev, active_input):
-    if ev.key != 'Enter':
+    if ev.key != "Enter":
         return
     draw_answer(active_input)
     clear_feedback()
     draw_feedback()
 
+
 def draw_challenge_blanks_mode(current_text, gap):
-    tbox = browser.document.getElementById('textbox')
-    p = browser.document.createElement('p')
+    tbox = browser.document.getElementById("textbox")
+    p = browser.document.createElement("p")
     for i in range(len(current_text)):
         if i == gap:
-            el = browser.document.createElement('input')
-            el.bind('keypress', lambda ev: user_input(ev, el))
-            el['id'] = 't' + str(i)
-            el['data-focus'] = 'true'
+            el = browser.document.createElement("input")
+            el.bind("keypress", lambda ev: user_input(ev, el))
+            el["id"] = "t" + str(i)
+            el["data-focus"] = "true"
             # TODO: this should actually be a class:
-            el['style'] = 'font-size: 110%; border: thin dotted #000; '
-            el['style'] += f'width: {len(current_text[i])}ch'
-            el['data-value'] = current_text[i]
+            el["style"] = "font-size: 110%; border: thin dotted #000; "
+            el["style"] += f"width: {len(current_text[i])}ch"
+            el["data-value"] = current_text[i]
             p.attach(el)
-            p.attach(' ')
+            p.attach(" ")
         else:
-            p.attach(current_text[i] + ' ')
+            p.attach(current_text[i] + " ")
     tbox.attach(p)
 
 
 def init_audio_player(locale, audio_path):
-    player = browser.document.getElementById('player')
-    source = browser.document.getElementById('audioSource')
+    player = browser.document.getElementById("player")
+    source = browser.document.getElementById("audioSource")
     source.src = (
-        '/static/cv-corpus-6.1-2020-12-11/' + locale + '/clips/' + audio_path
+        "/static/cv-corpus-6.1-2020-12-11/" + locale + "/clips/" + audio_path
     )
     player.load()
-    player.bind('ended', on_audio_ended)
+    player.bind("ended", on_audio_ended)
+
 
 def draw_clip(clip):
-    current_text = clip['question']['tokenized']
-    gap = clip['gap']
-    init_audio_player(clip['question']['locale'], clip['question']['path'])
+    current_text = clip["question"]["tokenized"]
+    gap = clip["gap"]
+    init_audio_player(clip["question"]["locale"], clip["question"]["path"])
     draw_challenge_blanks_mode(current_text, gap)
+
 
 def main():
     draw_languages()
     draw_feedback()
     get_clip_and_then(draw_clip)
+
 
 main()
