39659110aa550d507d36363b13a3df4fbaab0830
Francis Tyers <ftyers@prompsit.com>
2021-03-15 21:46:20 +0000

    add super basic taggers for proper nouns and punctuation: to be replaced

---
+++ lib/taggers.py
@@ -0,0 +1,48 @@
+import re
+
+def deu(sentence):
+    """
+        >>> deu(['Ich', 'habe', 'schlechte', 'Nachrichten', 'für', 'ihn', '.'])
+        ['X', 'X', 'X', 'X', 'X', 'X', 'PUNCT']
+    """
+    tags = []
+    first = True
+    for token in sentence:
+        if re.match('^[^\w+]+$', token):
+            tags.append('PUNCT')
+        else:
+            tags.append('X')
+        first = False
+
+    return tags
+
+
+def default(sentence):
+    """
+        >>> default(['This', 'is', 'not', 'Crewe', '.'])
+        ['X', 'X', 'X', 'PROPN', 'PUNCT']
+    """
+    tags = []
+    first = True
+    for token in sentence:
+        if re.match('^[^\w+]+$', token):
+            tags.append('PUNCT')
+        elif token[0] == token[0].upper() and not first:
+            tags.append('PROPN')
+        else:
+            tags.append('X')
+        first = False
+
+    return tags
+
+
+def tokenise(sentence, lang):
+    if lang in ["de", "deu"]:
+        return deu(sentence)
+
+    return default(sentence)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
